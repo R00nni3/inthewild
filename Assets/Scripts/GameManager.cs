5e7e0abcbd3782e10f6b9b1f2cebcaaed06a9d8a
R00nni3 <rosariog.perucich@gmail.com>
2026-09-03 23:24:30 -0400

    Arreglo de los carteles de start, game over y victory

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -83,4 +83,13 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
 }
